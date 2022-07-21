ffd3a8b310c6565ca0437f79de3bad77fcf6279d
Alec Larsen <hello@alec.ninja>
2022-07-21 14:06:07 -0700

    Just redirect to Github for now

--- config.rb
+++
@@ -1,9 +0,0 @@
-activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
-end
-
-configure :build do
-  activate :minify_css
-  activate :minify_javascript
-  activate :gzip
-end
--- source/javascripts/main.js
+++
@@ -1,9 +0,0 @@
-if ('addEventListener' in window) {
-    window.addEventListener('load', function() {
-      document.body.className = document.body.className.replace(/\bis-loading\b/, '');
-    });
-
-    if(navigator.userAgent.match(/(MSIE|rv:11\.0)/)) {
-      document.body.className += ' is-ie';
-    }
-}
